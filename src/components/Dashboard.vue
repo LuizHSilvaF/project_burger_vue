db8d55350e5f2fc57a2c4bfc7d290598bd98d639
LuizHSilvaF <luiz.henriqueoff10@gmail.com>
2023-09-14 12:29:53 -0300

    getPedios e deleteBurger created

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -11,21 +11,25 @@
             </div>
         </div>
         <div id="burger-table-rows">
-            <div class="burger-table-row">
-                <div class="order-number">1</div>
-                <div>João</div>
-                <div>Pão</div>
-                <div>Maminha</div>
+            <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+                <div class="order-number">{{burger.id}}</div>
+                <div>{{burger.nome}}</div>
+                <div>{{burger.pao}}</div>
+                <div>{{burger.carne}}</div>
                 <div>
                     <ul>
-                        <li>Salame</li>
+                        <li v-for="(opcional, index) in burger.opcionais" :key="index">
+                            {{opcional}}
+                        </li>
                     </ul>
                 </div>
                 <div>
                     <select name="status" class="status">
-                    <option value="">Selecione o status do pedido</option>
+                        <option value="s.tipo" v-for="s in status" :key="s.id" :selected="burger.status == s.tipo">
+                            {{s.tipo}}
+                        </option>
                     </select>
-                    <button class="delete-btn">Cancelar</button>
+                    <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
                 </div>
             </div>
         </div>
@@ -34,7 +38,41 @@
 
 <script>
     export default {
-        name: 'Dashboard'
+        name: 'Dashboard',
+        data(){
+            return {
+                burgers: null,
+                burger_id: null,
+                status: []
+            }
+        },
+        methods: {
+            async getPedidos() {
+                const req = await fetch("http://localhost:3000/burgers");
+                const data = await req.json();
+
+                this.burgers = data;
+            },
+            async getStatus() {
+                const req = await fetch("http://localhost:3000/status");
+                const data = await req.json();
+
+                this.status = data;
+            },
+            async deleteBurger(id) {
+                const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+                    method: "DELETE"
+                });
+
+                const res = await req.json();
+
+                this.getPedidos();
+            }
+        },
+        mounted() {
+            this.getPedidos();
+            this.getStatus();
+        }
 }
 </script>
 
